93571aca2a2f7620a7b66caf4b9287a2c5b81b51
dothuthanh <55688858+dothuthanh@users.noreply.github.com>
2020-04-14 20:48:40 +0700

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,57 @@
+import com.sun.tools.javac.Main;
+
+import java.util.Random;
+
+public class Car implements Runnable {
+    public static int DISTANCE = 100;
+    public static int STEP = 2;
+    private String name;
+    public Car(String name){
+        this.name = name;
+    }
+    @Override
+    public void run() {
+        int runDistance = 0;
+        long startTime = System.currentTimeMillis();
+        while (runDistance < DISTANCE) {
+            try {
+                int speed = (new Random()).nextInt(20);
+                runDistance += speed;
+                String log = "|";
+                int percentTravlel = (runDistance * 100) / DISTANCE;
+                for (int i = 0; i < DISTANCE; i += STEP) {
+                    if (percentTravlel >= i + STEP) {
+                        log += "=";
+                    } else if (percentTravlel >= i && percentTravlel < i + STEP) {
+                        log += "o";
+                    } else {
+                        log += "-";
+                    }
+                }
+                log += "|";
+                System.out.println("Car" + this.name + ": " + log + " " + Math.min(DISTANCE, runDistance) + "KM");
+                Thread.sleep(1000);
+            }catch (InterruptedException e){
+                System.out.println("Car" + this.name + " broken...");
+                break;
+            }
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Car" + this.name + " Finish in " + (endTime - startTime) /1000 + "s");
+    }
+    public static void main(String[] args) {
+        Car carA = new Car("A");
+        Car carB = new Car("B");
+        Car carC = new Car("C");
+
+        Thread thread1 = new Thread(carA);
+        Thread thread2 = new Thread(carB);
+        Thread thread3 = new Thread(carC);
+
+        System.out.println("Distance: 100KM");
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+
+}
